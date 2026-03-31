326d4af22545a8df3772810ea8bd88bc9bb621fe
sathwik <ssathwik31831@gmail.com>
2026-03-31 20:15:57 +0530

    Fix MySQL remnants and remove all workflows

--- backend/src/main/java/com/sathwik/auth/auth_service/AuthServiceApplication.java
+++ backend/src/main/java/com/sathwik/auth/auth_service/AuthServiceApplication.java
@@ -9,12 +9,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class AuthServiceApplication {
 	public static void main(String[] args) {
 		System.setProperty("java.net.preferIPv4Stack", "true");
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("DRIVER FOUND!");
-		} catch (ClassNotFoundException e) {
-			System.out.println("DRIVER NOT FOUND: " + e.getMessage());
-		}
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 }
